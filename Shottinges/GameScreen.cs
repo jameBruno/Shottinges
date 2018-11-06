88bcc2d345c5cd0ba9d001ee07f386ae39ee58f6
byeong9935 <byeong9935@gmail.com>
2018-11-06 14:26:33 +0900

    Default Programs

---
+++ Shottinges/GameScreen.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Input;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.ComponentModel;
+using OpenCvSharp;
+using OpenCvSharp.UserInterface;
+using OpenCvSharp.Extensions;
+using System.Threading;
+
+
+namespace Shooting
+{
+    class GameScreen
+    {
+
+        Program_Frame main;
+
+        int cnt, gamecnt;
+
+        int x, y;
+
+        Image dblBuff;
+
+        Graphics gc;
+
+
+        public Image bg;                            //배경화면
+        public Image bg_f;
+        public Image[] cloud = new Image[5];           //구름
+        public Image title;                         //타이틀화면
+        public Image title_key;
+
+        public Image[] chr = new Image[9];
+        public Image[] enemy = new Image[5];
+        public Image[] bullet = new Image[5];
+        public Image explo;
+        public Image[] item = new Image[3];
+
+        public Image _start;
+        public Image _over;
+        public Image shield;
+
+        public Font font;
+
+        public int[] v = { -2, -1, 0, 1, 2, 1, 0, -1 };
+        public int[] v2 = { -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7 };
+        public int step = 0;
+
+
+        public GameScreen(Program_Frame main)
+        {
+            this.main = main;
+            font = new Font("Default", 12, FontStyle.Bold);
+        }
+
+        public void Paint(Graphics g)
+        {
+            if (gc == null)
+            {
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Shottinges/Program_Frame.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace Shooting
+{
+    public partial class Program_Frame : Form
+    {
+
+        //public const ContextStaticAttribute int UP_PRESSED = 0x001;
+
+        public static int UP_PRESSED = 0x001;
+        public static int DOWN_PRESSED = 0x002;
+        public static int LEFT_PRESSED = 0x004;
+        public static int RIGHT_PRESSED = 0x008;
+        public static int FIRE_PRESSED = 0x010;
+
+        // 스레드 객체
+        Thread mainwork;
+
+        Boolean loof = true;
+
+        Random rnd = new Random();
+
+        public int status;
+        public int cnt;
+        public int delay;
+        public long pretime;
+        public int keybuff;
+
+        // 게임용 변수
+
+        public int score;
+        public int mylife;
+        public int gamecnt;
+        public int scrspeed = 16;
+        public int level;
+
+        public int myx, myy;
+        public int myspeed;
+        public int mydegree;
+
+        public int mywidth, myheight;
+        public int mymode = 1;
+        public int myimg;
+        public int mycnt;
+        public Boolean myshoot = false;
+        public int myshield;
+
+        public int gScreenWidth = 640;
+        public int gScreenHeight = 480;
+
+        public ArrayList bullets = new ArrayList();
+        public ArrayList enemies = new ArrayList();
+        public ArrayList effects = new ArrayList();
+        public ArrayList items = new ArrayList();
+
+
+
+
+
+
+        Program_Frame()
+        {
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+        // 플레이어
+        private Point Player;
+
+        private Point Enemy;
+
+        private Point Zig_Enemy;
+
+        // 점수 증가
+        private Point Support;
+
+        private int Score = 0;
+
+        Random random;
+
+    
+        public Program_Frame()
+        {
+            // 플레이어 좌표값 초기화
+            Player.X = 150;
+            Player.Y = 350;
+
+            Enemy.X = 100;
+            Enemy.Y = 0;
+
+            Zig_Enemy.X = 200;
+            Zig_Enemy.Y = 0;
+
+            Support.X = 300;
+            Support.Y = 0;
+
+            random = new Random();
+
+
+        }
+
+
+
+
+
+    
+
+    }
+
+
+}
